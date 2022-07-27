cc56e19fc28f2fc951b532926bf91ecec946ddf9
Django-TOPS <djangotestmail2021@gmail.com>
2022-07-27 15:33:54 +0530

    Auth Add

--- BatchProject/myapp/models.py
+++ BatchProject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class signup_master(models.Model):
+    created=models.DateTimeField(auto_now_add=True)
+    firstname=models.CharField(max_length=20)
+    lastname=models.CharField(max_length=20)
+    username=models.CharField(max_length=20)
+    password=models.CharField(max_length=12)
+    state=models.CharField(max_length=20)
+    city=models.CharField(max_length=20)
+    zipcode=models.IntegerField()
+
+    
--- BatchProject/myapp/views.py
+++ BatchProject/myapp/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import signupForm
+from .models import signup_master
 
 # Create your views here.
 
+def userSignup(request):
+    newuser=signupForm(request.POST)
+    if newuser.is_valid():
+        newuser.save()
+        print("User created successfully!")
+    else:
+        print(newuser.errors)
+        
+def userSignin(request):
+    unm=request.POST["username"]
+    pas=request.POST["password"]
+
+    user=signup_master.objects.filter(username=unm,password=pas)
+    if user: #true
+        print("Login successfully!")
+    else:
+        print("Error....Login fail, Try again.")
+
+
 def index(request):
+    if request.method=="POST":
+        if request.POST.get("login")=="login":
+            userSignin(request)
+            return redirect("notes")
+        elif request.POST.get("signup")=="signup":
+            userSignup(request)
+            return redirect("notes")
     return render(request,'index.html')
 
 def notes(request):
